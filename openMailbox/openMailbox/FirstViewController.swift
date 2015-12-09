00357e4e21717d6192d7b3f97648303e3ce2349c
f0go <f0go@protonmail.com>
2015-12-09 19:36:17 -0300

    [Main] Load labels

--- openMailbox/openMailbox/FirstViewController.swift
+++ openMailbox/openMailbox/FirstViewController.swift
@@ -9,11 +9,19 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-
+    
+    @IBOutlet weak var header: UIView!
     @IBOutlet weak var instagramPhoto: UIImageView!
+    @IBOutlet weak var navBarSectionsHolder: UIView!
+    @IBOutlet weak var selectedSectionLabel: UILabel!
+    
+    var navBarSections: NavBarSections!
+    
+    let labelsArray = ["uno", "dos", "un label mucho mas largo", "algo mas", "que mierda pasa", "que no funciona", "bien"]
     
     override func viewDidLoad() {
         getInstagramImage()
+        loadLabels()
     }
     
     func getInstagramImage(){
@@ -22,14 +30,28 @@ class FirstViewController: UIViewController {
                 print(error)
                 return
             }
-            
             let imageURL = json["data"][0]["images"]["standard_resolution"]["url"].stringValue
             
             Utils.getImageFromUrl(imageURL, callback: { (image) -> Void in
                 self.instagramPhoto.image = image
             })
-            
         }, retryCount: 0)
+    }
+    
+    func loadLabels() {
+        loadSections(labelsArray, selectedTextColor: "ffffff", textColor: "D2D2D2")
+    }
+    
+    func loadSections(elements: [String], selectedTextColor: String, textColor: String) {
+        navBarSections = UIStoryboard(name: "NavBarSections", bundle: nil).instantiateViewControllerWithIdentifier("NavBarSections") as! NavBarSections
+        navBarSections.view.frame = navBarSectionsHolder.frame
+        navBarSections.elements = elements
+        navBarSections.textColor = textColor
+        navBarSections.textColorSelected = selectedTextColor
+        navBarSections.callback = {(index, title) in
+            self.selectedSectionLabel.text = title
+        }
+        header.addSubview(navBarSections.view)
     }
 }
 
