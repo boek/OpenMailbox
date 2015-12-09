6cf434e8bd2bf1cf2939ddc16d24c520a4788a55
f0go <f0go@protonmail.com>
2015-12-09 18:04:34 -0300

    [Libs] Add SwiftyJSON submodule

--- openMailbox/openMailbox/Utils.swift
+++ openMailbox/openMailbox/Utils.swift
@@ -1,17 +1,14 @@
 //
-//  FirstViewController.swift
+//  Utils.swift
 //  openMailbox
 //
 //  Created by f0go on 12/9/15.
 //  Copyright © 2015 f0go. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class FirstViewController: UIViewController {
-
-    override func viewDidLoad() {
-
-    }
+class Utils {
+    
 }
-
